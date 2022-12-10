09288575ffeb688b63010826270c379fea26fb60
HSodhani1 <sodhani.h@northeastern.edu>
2022-12-10 14:48:14 -0500

    Directories & getter setters

---
+++ src/Business/Model/Network.java
@@ -0,0 +1,40 @@
+package Business.Model;
+
+/**
+ *
+ * @author Hardik
+ */
+
+public class Network {
+
+    private String name;
+    private EnterpriseDirectory enterpriseDirectory;
+
+    public Network(String name) {
+        this.name = name;
+        enterpriseDirectory = new EnterpriseDirectory();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+
+    public void setEnterpriseDirectory(EnterpriseDirectory enterpriseDirectory) {
+        this.enterpriseDirectory = enterpriseDirectory;
+    }
+   
+
+    public String toString()
+
+    {
+        return name;
+    }
+}
